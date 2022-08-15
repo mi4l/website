d7fd01577a3dc8986481452ee659bb5c01c764e3
mskalandunas <mskalandunas@gmail.com>
2022-08-14 19:33:06 -0700

    start on sequencer

---
+++ sequencer/index.js
@@ -0,0 +1,10 @@
+const BEATS_PER_MEASURE = 4;
+const MEASURES = 4;
+const BEATS_PER_SEQUENCE = BEATS_PER_MEASURE * MEASURES;
+
+// sequencing function
+class Sequencer {
+  constructor() {
+
+  }
+}
