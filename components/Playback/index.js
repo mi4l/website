941ea3aad6c0e48e83a8bfa4c3cb1a54fa4f0b4e
mskalandunas <mskalandunas@gmail.com>
2022-09-26 21:09:25 -0700

    basic playback controls class

---
+++ components/Playback/index.js
@@ -0,0 +1,32 @@
+class PlaybackControls {
+  #eventBus;
+
+  #EVENT_TYPES = {
+    PAUSE: 'playback.pause',
+    PLAY: 'playback.play',
+    STOP: 'playback.stop'
+  };
+
+  #PLAYBACK_STATES = {
+    PAUSE: 'PAUSE',
+    PLAY: 'PLAY',
+    STOP: 'STOP'
+  };
+
+  contructor(eventBus) {
+    this.#eventBus = eventBus;
+    this.#state = this.#PLAYBACK_STATES.STOP;
+  }
+
+  pause() {
+    this.#eventBus.dispatch(this.#EVENT_TYPES.PAUSE);
+  }
+
+  play() {
+    this.#eventBus.dispatch(this.#EVENT_TYPES.PLAY);
+  }
+
+  stop() {
+    this.#eventBus.dispatch(this.#EVENT_TYPES.STOP);
+  }
+}
